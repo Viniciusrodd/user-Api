9514eaf27a705ba9d1245f225df2f83905a0cd79
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-12-09 15:13:42 -0300

    Criando conexão knex com bd + update no .gitignore

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,10 @@ var app = express();
 var router = express.Router();
 var HomeController = require("../controllers/HomeController");
 
+
+
 router.get('/', HomeController.index);
+
+
 
 module.exports = router;
