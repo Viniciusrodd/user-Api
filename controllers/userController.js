6f8e8d9dc3fe3c1b49aece2a51c3ea51b5a49c81
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-12-10 11:45:43 -0300

    Criando validações para endpoint de criação de users

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,8 +5,18 @@ class userController{
     async index(req, res){}
 
     async create(req, res){
+        var { name, email, password } = req.body;
+
+        if (name === '' || email === '' || password === '') {
+            return res.status(400).send('Bad request my friend: Some fields are EMPTY');
+        }
+        
+        if (name === undefined || email === undefined || password === undefined) {
+            return res.status(403).send('Its UNDEFINED my friend');
+        }
+
         console.log(req.body);
-        res.send('Pegando o corpo da req');
+        return res.status(200).send('Its OK my friend');
     }
 }
 
